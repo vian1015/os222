c107e7c31166b400859524780f828d1823f51015
Jovian Marvin Chrisniady <112380572+vian1015@users.noreply.github.com>
2022-09-17 23:42:42 +0700

    week 2 update

--- TXT/myscript.sh
+++ TXT/myscript.sh
@@ -12,7 +12,7 @@
 # You new to set "REC2" with your own Public-Key Identity!
 # Check it out with "gpg --list-key"
 # ####################### Replace REC2 ####
-REC2="60914D29C01C81F1"
+REC2="F096A940222F0AE5"
 # ####################### ####### #### ####
 # REC1: public key
 REC1="63FB12B215403B20"
